a9599986baf9ccf6cac2c9cb764e280522ba14a2
rey-official <karemathenge8@gmail.com>
2024-11-11 13:36:16 +0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,11 @@
+// JavaScript for adding interactivity if needed
+
+// Example: Smooth Scroll for Internal Links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
